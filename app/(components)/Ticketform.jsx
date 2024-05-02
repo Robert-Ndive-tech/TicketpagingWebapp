82bf8f3299aab0234c592a645ba85671b720917c
robert <robertndive29@gmail.com>
2024-05-02 17:47:29 +0100

    messagingtestingstarting

--- app/(components)/Ticketform.jsx
+++ app/(components)/Ticketform.jsx
@@ -25,9 +25,17 @@ const TicketForm = () => {
     }));
   };
 
+  const handleSubmit = () => {
+    console.log("submitted");
+  };
+
   return (
     <div className="flex justify-center">
-      <form>
+      <form
+        className="flex flex-col gap-3 w-1/2"
+        method="POST"
+        onSubmit={handleSubmit}
+      >
         <h3>Create Your Ticket</h3>
         <label>Title</label>
         <input
@@ -38,6 +46,92 @@ const TicketForm = () => {
           required={true}
           value={formData.title}
         />
+        <label>Description</label>
+        <input
+          id="description"
+          name="description"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+          rows="5"
+        />
+        <label>Category</label>
+        <select
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+        >
+          <option value="Hardware_Problem">Hardware Problem</option>
+          <option value="Software_Problem">Software Problem</option>
+          <option value="Project">Project</option>
+        </select>
+
+        <label>Priority</label>
+        <div>
+          <input
+            id="priority-1"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={1}
+            checked={formData.priority == 1}
+          />
+          <label>1</label>
+          <input
+            id="priority-2"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={2}
+            checked={formData.priority == 2}
+          />
+          <label>2</label>
+          <input
+            id="priority-3"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={3}
+            checked={formData.priority == 3}
+          />
+          <label>3</label>
+          <input
+            id="priority-4"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={4}
+            checked={formData.priority == 4}
+          />
+          <label>4</label>
+          <input
+            id="priority-5"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={5}
+            checked={formData.priority == 5}
+          />
+          <label>5</label>
+        </div>
+        <label>Progress</label>
+        <input
+          type="range"
+          id="progress"
+          name="progress"
+          value={formData.progress}
+          min={0}
+          max={100}
+          onChange={handleChange}
+        />
+        <label>Status</label>
+        <select name="status" value={formData.status} onChange={handleChange}>
+          <option value="not_started">Not Started</option>
+          <option value="started">Started</option>
+          <option value="done">Done</option>
+        </select>
+        <input type="submit" className="btn" value="Create Ticket" />
       </form>
     </div>
   );
